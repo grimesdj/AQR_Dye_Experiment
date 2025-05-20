c1dd7fd589fd1e7762294222afb248bee40a1081
jkr6136 <jkr6136@uncw.edu>
2025-05-20 15:15:05 -0400

    Change Matlab file loc

---
+++ Rooker_GettingStarted/scratch/Untitled2.m
@@ -0,0 +1,6 @@
+% Load the L0 data
+load("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\data\Release1\L0\KELP1_AquadoppHR_L0.mat")
+% time to plot the b1:b3 data as speed vs time (remeber time is in decimal
+% days)
+%columns are bins (depth), rows are time at 600 second intervals, and
+%values are velocity
