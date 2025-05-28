ea0f740ea17715ce02253171ca3ac8412d4ed381
jkr6136 <jkr6136@uncw.edu>
2025-05-28 11:28:13 -0400

    set code to run all bins

--- Rooker_GettingStarted/scratch/plotRelease1.m
+++ Rooker_GettingStarted/scratch/plotRelease1.m
@@ -10,7 +10,7 @@ load("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\data\Release1\L0\KELP
 
 
 dye = find((date>=datenum('03-Jul-2024 18:38:00')) & (date>=datenum('03-Jul-2024 19:52:00')));
- for bindex = 1:1
+ for bindex = 1:size(b1,2)
      % Define B, A, C, as matrix of seconds x beam:
      B = [v1(dye,bindex), v2(dye,bindex), v3(dye,bindex)];
      A = [a1(dye,bindex), a2(dye,bindex), a3(dye,bindex)];
