344cd7284a31b405fc839ce15b0a25b7fa0608d5
jkr6136 <jkr6136@uncw.edu>
2025-06-06 13:53:47 -0400

    fixing the M1 script

--- Rooker_GettingStarted/scratch/finding_M1_times.m
+++ Rooker_GettingStarted/scratch/finding_M1_times.m
@@ -6,25 +6,3 @@ files = dir("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\data\2024_PROC
 
 TRange = readtable("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\info\dye_mixing_cals_and_releases\dye_release_times.csv");
 
-
-ADCP = struct();
-M1 = struct();
-for i = 1:length(files)
-    ind = sprintf('M1_%d', i);
-    if ~contains(files(i).name, 'config')  &&  ~contains(files(i).name, 'min')
-        ADCP.(ind) = load([files(i).folder,filesep,files(i).name],'Time');
-        for  releaseNum = 1:3
-            Num = sprintf('release%d', releaseNum);
-            time.(Num) = datenum(TRange.StartTime_UTC_(releaseNum));
-            if any(ADCP.M1_1.Time >= datenum(TRange.StartTime_UTC_(releaseNum)) & ADCP.M1_1.Time <= datenum(TRange.EndTime_UTC_(releaseNum)))
-                M1.(Num) = load([files(i).folder,filesep,files(i).name]);
-            end
-            
-
-        end
-        
-    
-    end
-    
-end
-
---
+++ Rooker_GettingStarted/scratch/finding_M1_times_Null.m
@@ -0,0 +1,32 @@
+
+
+% I dont know if this returns the right thing 
+
+
+files = dir("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\data\2024_PROCESSED_DATA\M1\L0\ADCP\ADCP_M1_*.mat");
+
+TRange = readtable("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\info\dye_mixing_cals_and_releases\dye_release_times.csv");
+
+
+ADCP = struct();
+M1 = struct();
+for i = 1:length(files)
+    ind = sprintf('M1_%d', i);
+    if ~contains(files(i).name, 'config')  &&  ~contains(files(i).name, 'min')
+        ADCP.(ind) = load([files(i).folder,filesep,files(i).name],'Time');
+        for  releaseNum = 1:3
+            Num = sprintf('release%d', releaseNum);
+            time.(Num) = datenum(TRange.StartTime_UTC_(releaseNum));
+            if any(ADCP.(ind).Time >= datenum(TRange.StartTime_UTC_(releaseNum)) & ADCP.M1_1.Time <= datenum(TRange.EndTime_UTC_(releaseNum)))
+                M1.(Num) = load([files(i).folder,filesep,files(i).name]);
+                disp([Num ' corresponds to ' ind])
+            end
+            
+
+        end
+        
+    
+    end
+    
+end
+
