25164a491faaab2816da3b0bed3abdd13d19076f
jkr6136 <jkr6136@uncw.edu>
2025-06-11 16:43:16 -0400

    getting stats tables together

--- Rooker_GettingStarted/scratch/XYZtoENU.m
+++ Rooker_GettingStarted/scratch/XYZtoENU.m
@@ -1,7 +1,19 @@
 % converting Vector XYZ to ENU matching the orientation of Aquadopp
 
 % get aquadopp orietation
-headAQD = load("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\data\Release1\raw\KELP1_AquadoppHR_raw.mat", 'head');
+%headAQD = load("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\data\Release1\raw\KELP1_AquadoppHR_raw.mat", 'head');
 
 % get Vector data
-Vector = load("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\data\Release1\L0\KELP1_Vector_L0.mat");
+%Vector = load("C:\Users\jkr6136\OneDrive - UNC-Wilmington\Kelp_data\data\Release1\L0\KELP1_Vector_L0.mat");
+function rotation(theta, phi, omega)
+st = sind(theta);
+sp = sind(phi);
+so = sind(omega);
+ct = cosd(theta);
+cp = cosd(phi);
+co = cosd(omega);
+Head = [st ct 0; -(ct) st 0; 0 0 1]
+P = [ 0 cp sp; -1 0 0; 0 -(sp) cp]
+R = [0 1 0; -(co) 0 so; so 0 co]
+H*P*R
+end
