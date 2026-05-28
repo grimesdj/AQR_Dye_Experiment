543732e53af8c1034923194cc9e44a7c3719ef5f
jkr6136 <jkr6136@uncw.edu>
2026-05-28 16:53:14 -0400

    making the perfect little pretty picture so it looks like i know what im doing

--- Rooker_GettingStarted/scratch/plot_LPF_profiles.m
+++ Rooker_GettingStarted/scratch/plot_LPF_profiles.m
@@ -14,4 +14,23 @@ depTime  = [datenum('03-Jul-2024 18:30:00'), datenum('03-Jul-2024 22:30:00') ;
 
 M1.ADCP.Time = M1.ADCP.Time(M1.ADCP.Time >= depTime(1, releasenum) & M1.ADCP.Time <= depTime(2, releasenum));
 M1.Moor.Time = M1.Moor.Time(M1.Moor.Time >= depTime(1, releasenum) & M1.Moor.Time <= depTime(2, releasenum));
-Time = interp1(M1.ADCP.Moor, M1.Moor.
+Time = interp1(M1.ADCP.Moor, M1.Moor.Time);
+
+
+% plot
+figure
+img = imagesc(datetime(M1.ADCP.Time, 'convertfrom', 'datenum'), M1.ADCP.bin_mab, M1.LPF.Velocity_North');
+set(img, 'AlphaData', ~isnan(M1.LPF.Velocity_North'))
+set(gca, 'YDir', 'normal')
+cb = colorbar;
+colormap(cmocean('balance'))
+clim([-0.1 0.1])
+ylabel('Meters Above Bottom')
+ylabel(cb, 'Cross-shore Velocity, $u$ [m/s]', 'Interpreter','latex', 'FontSize', 18)
+set([gca cb], 'fontsize', 18)
+ylim([0 max(M1.ADCP.maxRNG)+std(M1.ADCP.maxRNG)])
+
+
+
+
+
